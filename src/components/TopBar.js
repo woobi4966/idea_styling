10229855f793138d65ba46f22b5b143ff1f5edda
AnJungMin <dkswjdals4027@gmail.com>
2022-04-27 17:42:41 +0900

    Topbar Update - jungmin

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,12 +1,15 @@
-import css from './TopBar.module.css';
+<body topmargin="0" bottommargin="0" leftmargin="0" rightmargin="0" style="background-color:#F6F6F6">
+    <div class="menu" >
+      <nav class="clearfix">
+        <ul class="clearfix">
+          <li><a href="http://www._____.com">IDEA STYLING</a></li>
+          <li><a href="http://www._____.com/CreatCard">New</a></li>
+          <li><a href="http://www._____.com/Share">Share</a></li>
+          <li><a href="http://www._____.com/Save">Save</a></li>
+          <li><a href="http://www._____.com/Settings">Settings</a></li>
+      </ul>
+      <a id="pull" href="#"></a>
+    </nav>
+    </div>
+  </body>
 
-function TopBar(){
-
-    return(
-        <div className={css.section}>
-            TopBar
-        </div>
-    );
-}
-
-export default TopBar;
