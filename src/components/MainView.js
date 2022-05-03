05bd8243250b5bf5b64d87b359c7b8b6cffa4850
Lee Jong Moon <woobi4966@naver.com>
2022-05-03 13:35:33 +0900

    completed width CSS option

--- src/components/MainView.js
+++ src/components/MainView.js
@@ -1,32 +1,27 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import css from './MainView.module.css';
 
-function getWindowDimensions() {
-    const { innerWidth: width, innerHeight: height} = window;
-    return (width);
-}
-
 export default function MainView(){
-
-    const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
-    // const [rewidth, setreWidth] = useState();
-
-    useEffect(() => {
-        function handleResize() {
-            setWindowDimensions(getWindowDimensions());
-        }
-
-        window.addEventListener("resize", handleResize);
-        return () => window.removeEventListener("resize", handleResize);
-
-        document.getElementById('section').style.width = windowDimensions;
-    }, []);
-
-
     
+    const [cssWidth, setCssWidth] = useState(window.innerWidth);
+    const [style, setStyle] = useState('');
+    
+    
+
+    // window의 크기가 변경되면 실행할 함수
+    function resize() {
+        // window width에서 양쪽 사이드 바를 제거한 width를 얻는다
+        setCssWidth(window.innerWidth - (280*2));
+        // 이를 css style를 위한 string으로 변환
+        // setStyle("width:"+cssWidth+'px');
+    }
+
+    // window의 크기가 변경되면 resize()를 실행하라
+    window.onresize = resize;
+
     return(
-        <div id='section' className={css.section} width={windowDimensions}>
-            <p>{windowDimensions}</p>    
+        <div id='section' className={css.section} style={{width: cssWidth + 'px'}}>
+            <p>{cssWidth}</p>    
         </div>
     );
 }
