ff848e8f323994205fa9f700acd1531b111cb0b1
Lee Jong Moon <woobi4966@naver.com>
2022-05-06 02:49:22 +0900

    changed CSS, added contenteditable div,added fontawesome, added react-markdown

--- src/components/MainView.js
+++ src/components/MainView.js
@@ -1,12 +1,16 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+// import ReactMarkdown from 'react-markdown';
+
 import css from './MainView.module.css';
 
 export default function MainView(){
     
-    const [cssWidth, setCssWidth] = useState(window.innerWidth);
-    const [style, setStyle] = useState('');
-    
-    
+    const [cssWidth, setCssWidth] = useState(window.innerWidth - (280*2));
+
+
+    ////////////////////////////////////
+    // 0. window 사이즈 자동 조절
+    ////////////////////////////////////
 
     // window의 크기가 변경되면 실행할 함수
     function resize() {
@@ -17,11 +21,36 @@ export default function MainView(){
     }
 
     // window의 크기가 변경되면 resize()를 실행하라
-    window.onresize = resize;
+
+    // window.onresize = resize;
+
+    useEffect(() => {
+        window.onresize = resize;
+    });
+
+    ////////////////////////////////////
+    // 1. board을 클릭하면 글을 쓸 수 있다
+    ////////////////////////////////////
+    let board = document.getElementsByClassName(css.board);
+
+    function addTextInput() {
+        // board.append(<div contenteditable='true' placeholder='type anything'></div>);
+    }
 
     return(
         <div id='section' className={css.section} style={{width: cssWidth + 'px'}}>
-            <p>{cssWidth}</p>    
+            <p style={{color: '#ffffff'}}>{cssWidth}</p>    
+
+            <div className={css.title}>
+                <p>Welcome</p>
+                <p>작성날짜 : 2022. 5. 5.</p>
+                <p>만든이 : author</p>
+            </div>
+            <div className={css.board} onClick={addTextInput} onChange={console.log('cange')}>
+                    <div contentEditable='true' placeholder='type anything'>
+                    </div>
+                    
+            </div>
         </div>
     );
 }
