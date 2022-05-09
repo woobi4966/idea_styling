cb838d1047453f3f13b16749cb0f1f74e35dab42
korea414 <kms0414237@gmail.com>
2022-05-10 02:21:06 +0900

    Component test

--- src/components/ComponentMenu.js
+++ src/components/ComponentMenu.js
@@ -1,11 +1,26 @@
 import css from './ComponentMenu.module.css';
+import React from 'react';
+import { Button, View, Text } from 'react';
+import { createStackNavigator, createAppContainer } from 'react';
 
-function ComponentMenu(){
-    return(
-        <div className={css.section}>
-            <h1>Component Menu</h1>
-        </div>
-    );
+
+
+const Button1 = (h1) => {
+    ComponentMenu.log(h1)
+}
+const Button2 = (h1) => {
+    ComponentMenu.log(h1)
+}
+const Button3 = (h1) => {
+    ComponentMenu.log(h1)
 }
 
+    function ComponentMenu(){
+        return(
+            <view>
+               
+            </view>
+        );
+    }
 export default ComponentMenu;
+
