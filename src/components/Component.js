d45e78530c80a1058993749161e55c71d476d170
korea414 <kms0414237@gmail.com>
2022-05-12 22:14:20 +0900

    the second branch, .vscode is deleted

---
+++ src/components/Component.js
@@ -0,0 +1,24 @@
+import css from './Component.module.css';
+
+function ComponentMenu_Button(disabled, children) {
+    return (
+        <div> className = { css.section }
+        <h1> Component Menu </h1> 
+        
+        <div class="5W1H">
+            <h1>5W1H</h1>
+            <Button1 disabled={disabled}>{children}</Button1>;
+        </div>
+        <div class="Intro_Sound">
+            <h1>Intro Sound</h1>
+            <Button2 disabled={disabled}>{children}</Button2>;
+        </div>
+        <div class="Background">
+            <h1>Background</h1>
+            <Button3 disabled={disabled}>{children}</Button3>;
+        </div>
+        </div>
+    );
+}
+
+export default ComponentMenu_Button;
---
+++ src/components/ComponentMenu_Section.js
@@ -0,0 +1,25 @@
+import css from './Component.module.css';
+
+function Component(){
+    <div class="ComponentMenu_section__udD-W">
+        <div>
+            <div class="Component_body__yqneB">
+                    <div class="Card_titleBox__rQ4BL">
+                        <h3>hello</h3>
+                    </div>
+                </div>
+            <div class="Component_body__yqneB">
+                    <div class="Card_titleBox__rQ4BL">
+                        <h3>hello</h3>
+                    </div>
+                </div>
+                    <div class="Component_body__yqneB">
+                            <div class="Card_titleBox__rQ4BL">
+                                <h3>hello</h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+}
+
+export default Component;
