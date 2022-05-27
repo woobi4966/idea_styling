29dd4d147963d185a44cb7b7e4acce6a6a96532c
jang <hirokatsu21116@gmail.com>
2022-05-27 09:46:41 +0900

    test server

--- src/components/CardMenu.js
+++ src/components/CardMenu.js
@@ -1,22 +1,33 @@
-import classnames from 'classnames';
+import classnames from "classnames";
+import Card from "./Card";
+import css from "./CardMenu.module.css";
+import { useState, useEffect, useCallback } from "react";
 
-import Card from './Card';
-import css from './CardMenu.module.css';
+function CardMenu() {
+  const [title, setTitle] = useState("");
 
-function CardMenu(){
-    return(
-        <div className={classnames(css.section)}>
-            {/* <h1>Card Menu</h1> */}
-            <div className={css.cards}>
-                <Card title="hello"/>
-                <Card title="hello"/>
-                <Card title="hello"/>
-                <Card title="hello"/>
-                <Card title="hello"/>
-                <Card title="hello"/>
-            </div>
-        </div>
-    );
+  const getPostTitle = useCallback(async () => {
+    const res = await fetch("http://localhost:3001/c_table");
+    const json = await res.json();
+    setTitle(json[0].title);
+  }, []);
+
+  useEffect(() => {
+    getPostTitle();
+  }, [getPostTitle]);
+  return (
+    <div className={classnames(css.section)}>
+      {/* <h1>Card Menu</h1> */}
+      <div className={css.cards}>
+        <Card title="hello" />
+        <Card title={title} />
+        <Card title="hello" />
+        <Card title="hello" />
+        <Card title="hello" />
+        <Card title="hello" />
+      </div>
+    </div>
+  );
 }
 
 export default CardMenu;
