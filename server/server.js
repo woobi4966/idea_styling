a96c1fa7d0f07661d7ae802be0f77ee2f36ab0bd
Lee Jong Moon <woobi4966@naver.com>
2022-06-02 00:08:23 +0900

    added spotify server

--- server/server.js
+++ server/server.js
@@ -1,9 +1,34 @@
-let http = require("http");
+var request = require('request'); // "Request" library
 
-  /* Create an HTTP server to handle responses */
+var client_id = '575bc87a18da4048a955b5f5660a8172'; // Your client id
+var client_secret = '9edca493cee34ecf9c8d28f654654292'; // Your secret
 
-  http.createServer(function(request, response) {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello World");
-    response.end();
-  }).listen(8888);
+// your application requests authorization
+var authOptions = {
+  url: 'https://accounts.spotify.com/api/token',
+  headers: {
+    'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+  },
+  form: {
+    grant_type: 'client_credentials'
+  },
+  json: true
+};
+
+request.post(authOptions, function(error, response, body) {
+  if (!error && response.statusCode === 200) {
+
+    // use the access token to access the Spotify Web API
+    var token = body.access_token;
+    var options = {
+      url: 'https://api.spotify.com/v1/users/jmperezperez',
+      headers: {
+        'Authorization': 'Bearer ' + token
+      },
+      json: true
+    };
+    request.get(options, function(error, response, body) {
+      console.log(body);
+    });
+  }
+});
